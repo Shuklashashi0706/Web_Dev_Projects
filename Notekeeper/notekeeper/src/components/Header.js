76bb07849c6c5aacf061b604a91efa1298a7fe0e
Shuklashashi0706 <shuklashashi169@gmail.com>
2023-06-22 23:16:47 +0530

    time update

--- Notekeeper/notekeeper/src/components/Header.js
+++ Notekeeper/notekeeper/src/components/Header.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useState} from 'react'
 function Header() {
+  const now=new Date().toLocaleTimeString();
+  const [time,setTime]=useState(now);
+  setInterval(updateTime,1000); 
+  function updateTime(){
+    const newTime= new Date().toLocaleTimeString();
+    setTime(newTime);
+}
   return (
     <>
     <div className='w-[100%]'>
@@ -9,6 +17,8 @@ function Header() {
       <ul>
         <li><Link to='/'>Home</Link></li>
       </ul>
+      <h1 className="">{time}</h1>
+
       </div>
     </div>
     </>
--- Notekeeper/notekeeper/src/screens/Main.js
+++ Notekeeper/notekeeper/src/screens/Main.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import notes from "../components/notes";
 import Card from "../components/Card";
 function Main() {
+ 
   function createNote(note) {
     return (
       <>
@@ -10,6 +11,7 @@ function Main() {
       </>
     );
   }
+  
   return (
     <>
       <div className=" h-screen m-2 border border-black flex  flex-wrap">
